661fc70d448a2862a59c3b64fc9372114e68152e
fernando <goncalves.fernandolopes@gmail.com>
2022-01-22 11:34:20 -0300

    Sistema de cadastro de peças completo

---
+++ pecas.js
@@ -0,0 +1,31 @@
+
+let pesoPeca = 150 
+
+
+if(pesoPeca > 100) {
+    console.log("Peso maior que 100g, logo, podemos cadastrar a peça")
+} else {
+    console.log("Peso insuficiente, não é possivel cadastrar")
+}
+
+let numeroPecas = 8
+
+if(numeroPecas < 10) {
+    console.log ("Ainda há espaço na caixa, podemos cadastrar a peça")
+} else {
+    console.log ("Não há espaço disponível na caixa")
+}
+
+let nomePeca = "Disco de Freio"
+
+
+
+console.log(" O comprimento do nome da peça é:", nomePeca.length)
+
+
+
+if(nomePeca.length < 3) {
+    console.log ("Nome de peça muito curto, não é possível cadastrar")
+} else{
+    console.log("Nome da peça adequado, podemos cadastrar")
+}
